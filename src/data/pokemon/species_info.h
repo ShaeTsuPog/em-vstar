74c20b1b679744e0456723a1e6253a0c84b20739
ShaeTsuPog <shaetsu@protonmail.com>
2023-04-14 14:50:40 -0700

    backup tiles

--- src/data/pokemon/species_info.h
+++ src/data/pokemon/species_info.h
@@ -21116,30 +21116,7 @@ const struct SpeciesInfo gSpeciesInfo[] =
         .noFlip = FALSE,
         .flags = SPECIES_FLAG_MEGA_EVOLUTION,
     },
-/*
-    [SPECIES_CHARIZARD_MEGA_Z] =
-    {
-        .baseHP        = 78,
-        .baseAttack    = 84,
-        .baseDefense   = 140,
-        .baseSpeed     = 90,
-        .baseSpAttack  = 115,
-        .baseSpDefense = 133,
-        .types = { TYPE_FIRE, TYPE_FIRE },
-        .catchRate = 45,
-        .expYield = 285,
-        .evYield_SpAttack = 3,
-        .genderRatio = PERCENT_FEMALE(12.5),
-        .eggCycles = 20,
-        .friendship = STANDARD_FRIENDSHIP,
-        .growthRate = GROWTH_MEDIUM_SLOW,
-        .eggGroups = { EGG_GROUP_MONSTER, EGG_GROUP_DRAGON },
-        .abilities = { ABILITY_IGNITION, ABILITY_IGNITION },
-        .bodyColor = BODY_COLOR_RED,
-        .noFlip = FALSE,
-        .flags = SPECIES_FLAG_MEGA_EVOLUTION,
-    },
-*/
+
     [SPECIES_BLASTOISE_MEGA] =
     {
         .baseHP        = 79,
