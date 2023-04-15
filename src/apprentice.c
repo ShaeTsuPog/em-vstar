6330f015440caff85896dcfe915de19468082f82
ShaeTsuPog <shaetsu@protonmail.com>
2023-04-15 16:01:04 -0700

    overworld expansion

--- src/apprentice.c
+++ src/apprentice.c
@@ -1199,7 +1199,7 @@ static void SaveApprentice(void)
 static void SetSavedApprenticeTrainerGfxId(void)
 {
     u8 i;
-    u8 objectEventGfxId;
+    u16 objectEventGfxId;
     u8 class = gApprentices[gSaveBlock2Ptr->apprentices[0].id].facilityClass;
 
     for (i = 0; i < ARRAY_COUNT(gTowerMaleFacilityClasses) && gTowerMaleFacilityClasses[i] != class; i++)
